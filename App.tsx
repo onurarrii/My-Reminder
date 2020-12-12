2ae4ea993fb5bab6e5bd1789b01f57a96a83746c
Onur ARI <onurarrii@hotmail.com>
2020-12-12 12:52:55 +0300

    Add splash screen support without IOS

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import store from './src/store/store';
 import { Provider } from 'react-redux';
 import Router from './src/router/Router';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 
 const App: React.FC<null> = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Provider store={store}>
